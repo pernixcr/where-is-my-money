65356efc3a493dbe5f6e45d245321059ceaf1f0a
Esteban Soto <esoto@pernix-solutions.com>
2013-02-02 08:55:09 -0600

    starting to search my money

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WhereIsMyMoney::Application.config.secret_token = 'a01552827f05373a660b65ba97dff9c43c8fb7d5b59bc0df0a6e7fcee1738df6ac870283ff53f2981ee3c1ca95e358eb0a55bd43f35c15cd5dc37e7a45120b62'
